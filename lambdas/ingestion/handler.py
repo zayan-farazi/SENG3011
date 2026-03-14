86fd508aca61948e1477caa31a1afd19a1aff1d0
TomerErlich <tomererlich06@gmail.com>
2026-03-15 10:09:56 +1100

    main code and unit test for ingestion microservice

---
+++ lambdas/ingestion/handler.py
@@ -0,0 +1,67 @@
+import json
+import boto3
+import requests
+import os
+from datetime import datetime, timezone
+import constants
+
+s3 = boto3.client("s3")
+bucket_name = os.environ.get("DATA_BUCKET")
+api_key = os.environ.get("API_KEY")
+
+
+def load_hubs():
+    s3_response = s3.get_object(Bucket=bucket_name, Key="hubs.json")
+    return json.loads(s3_response["Body"].read().decode("utf-8"))
+
+
+def fetch_weather(lat, lon):
+    url = f"https://api.pirateweather.net/forecast/{api_key}/{lat},{lon}"
+    querystring = {"exclude":"","extend":"","lang":"","units":"","version":"","tmextra":"","icon":""}
+
+    weather_data = requests.get(url, params=querystring)
+    weather_data.raise_for_status()
+    return weather_data.text
+
+def store_weather(hub_id, date, weather_data):
+    s3.put_object(
+        Bucket=bucket_name,
+        Key=f"raw/weather/{hub_id}/{date}.json",
+        Body=weather_data,
+        ContentType="application/json"
+    )
+
+def lambda_handler(event, context):
+    if not bucket_name:
+        return response(constants.STATUS_INTERNAL_SERVER_ERROR, {"error": "Missing DATA_BUCKET configuration"})
+    
+    if not api_key:
+        return response(constants.STATUS_INTERNAL_SERVER_ERROR, {"error": "Missing API key"})
+
+    hub_id = event.get("pathParameters") or {}
+    hub_id = hub_id.get("hubId")
+
+    hubs = load_hubs()
+ 
+    
+    if hub_id:
+        if hub_id not in hubs:
+            return response(constants.STATUS_BAD_REQUEST, {"error": "Invalid hub_id"})
+        hubs = {hub_id: hubs[hub_id]}
+
+    date = datetime.now().strftime(constants.DATE_FORMAT)
+
+    for hub_id, hub_data in hubs.items():
+        
+        weather_data = fetch_weather(hub_data["lat"], hub_data["lon"])
+        store_weather(hub_id, date, weather_data)
+
+    return response(constants.STATUS_OK, {"message": "Success"})
+
+def response(status, body):
+    return {
+        "statusCode": status,
+        "body": json.dumps(body)
+    }
+            
+    
---
+++ tests/unit/test_ingest_weather_data.py
@@ -0,0 +1,78 @@
+import json
+from unittest.mock import patch
+from lambdas.ingestion.handler import lambda_handler
+
+
+@patch("lambdas.ingestion.handler.store_weather")
+@patch("lambdas.ingestion.handler.fetch_weather")
+@patch("lambdas.ingestion.handler.load_hubs")
+def test_lambda_handler_success_single_hub(mock_load, mock_fetch, mock_store):
+
+    fake_event = {"pathParameters": {"hubId": "hub001"}}
+
+    mock_load.return_value = {
+        "hub001": {"lat": 10, "lon": 20},
+        "hub002": {"lat": 30, "lon": 40},
+    }
+
+    mock_fetch.return_value = '{"temperature":25}'
+
+    result = lambda_handler(fake_event, None)
+
+    mock_store.assert_called_once()
+    args = mock_store.call_args[0]
+
+    assert args[0] == "hub001"
+    assert args[2] == '{"temperature":25}'
+    assert result["statusCode"] == 200
+
+
+@patch("lambdas.ingestion.handler.store_weather")
+@patch("lambdas.ingestion.handler.fetch_weather")
+@patch("lambdas.ingestion.handler.load_hubs")
+def test_lambda_handler_all_hubs(mock_load, mock_fetch, mock_store):
+
+    fake_event = {}
+
+    mock_load.return_value = {
+        "hub001": {"lat": 10, "lon": 20},
+        "hub002": {"lat": 30, "lon": 40},
+    }
+
+    mock_fetch.return_value = '{"temperature":25}'
+
+    result = lambda_handler(fake_event, None)
+
+    assert mock_store.call_count == 2
+    assert result["statusCode"] == 200
+
+
+@patch("lambdas.ingestion.handler.load_hubs")
+def test_lambda_handler_invalid_hub(mock_load):
+
+    fake_event = {"pathParameters": {"hubId": "badHub"}}
+
+    mock_load.return_value = {
+        "hub001": {"lat": 10, "lon": 20}
+    }
+
+    result = lambda_handler(fake_event, None)
+
+    assert result["statusCode"] == 400
+    assert json.loads(result["body"])["error"] == "Invalid hub_id"
+
+
+@patch("lambdas.ingestion.handler.bucket_name", None)
+def test_lambda_handler_missing_bucket():
+
+    result = lambda_handler({}, None)
+
+    assert result["statusCode"] == 500
+
+
+@patch("lambdas.ingestion.handler.api_key", None)
+def test_lambda_handler_missing_api_key():
+
+    result = lambda_handler({}, None)
+
+    assert result["statusCode"] == 500
