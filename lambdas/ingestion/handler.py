74d6b59bf18160488989749263e036c55d39e3cc
TomerErlich <tomererlich06@gmail.com>
2026-03-16 07:21:39 +1100

    recommit without terraform file because I broke it accidently

--- lambdas/ingestion/handler.py
+++ lambdas/ingestion/handler.py
@@ -5,17 +5,13 @@ import os
 from datetime import datetime
 import constants
 
-s3 = boto3.client("s3")
-bucket_name = os.environ.get("DATA_BUCKET")
-api_key = os.environ.get("API_KEY")
 
-
-def load_hubs():
-    s3_response = s3.get_object(Bucket=bucket_name, Key="hubs.json")
+def load_hubs(s3, bucket_name):
+    s3_response = s3.get_object(Bucket=bucket_name, Key=constants.HUBS_FILE_KEY)
     return json.loads(s3_response["Body"].read().decode("utf-8"))
 
 
-def fetch_weather(lat, lon):
+def fetch_weather(lat, lon, api_key):
     url = f"https://api.pirateweather.net/forecast/{api_key}/{lat},{lon}"
     querystring = {"exclude":"","extend":"hourly","lang":"","units":"","version":"","tmextra":"","icon":""}
 
@@ -23,7 +19,7 @@ def fetch_weather(lat, lon):
     weather_data.raise_for_status()
     return weather_data.text
 
-def store_weather(hub_id, date, weather_data):
+def store_weather(s3, bucket_name, hub_id, date, weather_data):
     s3.put_object(
         Bucket=bucket_name,
         Key=f"raw/weather/{hub_id}/{date}.json",
@@ -32,6 +28,10 @@ def store_weather(hub_id, date, weather_data):
     )
 
 def lambda_handler(event, context):
+    s3 = boto3.client("s3")
+    bucket_name = os.environ.get("DATA_BUCKET")
+    api_key = os.environ.get("API_KEY")
+
     if not bucket_name:
         return response(constants.STATUS_INTERNAL_SERVER_ERROR, {"error": "Missing DATA_BUCKET configuration"})
     
@@ -39,9 +39,9 @@ def lambda_handler(event, context):
         return response(constants.STATUS_INTERNAL_SERVER_ERROR, {"error": "Missing API key"})
 
     hub_id = event.get("pathParameters") or {}
-    hub_id = hub_id.get("hubId")
+    hub_id = hub_id.get("hub_id")
 
-    hubs = load_hubs()
+    hubs = load_hubs(s3, bucket_name)
  
     
     if hub_id:
@@ -53,8 +53,8 @@ def lambda_handler(event, context):
 
     for hub_id, hub_data in hubs.items():
         
-        weather_data = fetch_weather(hub_data["lat"], hub_data["lon"])
-        store_weather(hub_id, date, weather_data)
+        weather_data = fetch_weather(hub_data["lat"], hub_data["lon"], api_key)
+        store_weather(s3, bucket_name, hub_id, date, weather_data)
 
     return response(constants.STATUS_OK, {"message": "Success"})
 
