d5312af44b89fce2d8ce416d1a6f7a50beedfe35
Hoang.Dang16 <Hoang.Dang16@MacBook-Pro.local>
2026-03-30 22:38:32 +1100

    fix lambda handler to treat tests not as module - avoid errors

--- lambdas/testing/handler.py
+++ lambdas/testing/handler.py
@@ -1,5 +1,15 @@
-from tests.e2e.test_e2e_full_pipeline import test_e2e_full_pipeline, test_e2e_wrong_date
 from constants import STATUS_OK
+import os
+import importlib.util
+
+# Use relative path to avoid module errors
+e2e_file = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../tests/e2e/test_e2e_full_pipeline.py"))
+spec = importlib.util.spec_from_file_location("test_e2e_full_pipeline", e2e_file)
+e2e_module = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(e2e_module)
+
+test_e2e_full_pipeline = e2e_module.test_e2e_full_pipeline
+test_e2e_wrong_date = e2e_module.test_e2e_wrong_date
 
 def lambda_handler(event, context):
     results = {}
