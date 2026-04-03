755f3fe341dce1039bce8543e3e7d3e397d2a934
Zayan Farazi <zayan.farazi1@gmail.com>
2026-04-03 12:04:29 +1100

    Revert "updated the testing handler"
    This reverts commit 4b123ac6da4be8866204b220da054b922fe2c83a.

--- lambdas/testing/handler.py
+++ lambdas/testing/handler.py
@@ -1,31 +1,20 @@
-import json
-
-from constants import STATUS_OK
 from tests.e2e.test_e2e_full_pipeline import test_e2e_full_pipeline, test_e2e_wrong_date
-
-
+from constants import STATUS_OK
 def lambda_handler(event, context):
     results = {}
-    overall_status = "pass"
-
     try:
         test_e2e_full_pipeline()
         results[test_e2e_full_pipeline.__name__] = "PASS"
     except AssertionError as e:
         results[test_e2e_full_pipeline.__name__] = f"FAIL: {str(e)}"
-        overall_status = "fail"
 
     try:
         test_e2e_wrong_date()
         results[test_e2e_wrong_date.__name__] = "PASS"
     except AssertionError as e:
         results[test_e2e_wrong_date.__name__] = f"FAIL: {str(e)}"
-        overall_status = "fail"
 
     return {
         "statusCode": STATUS_OK,
-        "body": json.dumps({
-            "status": overall_status,
-            "results": results,
-        }),
+        "body": results
     }
