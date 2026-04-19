aa6fe264d42190a8f3f3136cbfe7a5aff6fb06c8
TomerErlich <tomererlich06@gmail.com>
2026-04-19 17:24:50 +1000

    lint again

--- terraform/main.tf
+++ terraform/main.tf
@@ -382,12 +382,12 @@ resource "aws_dynamodb_table" "scores" {
   name         = local.scores_table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "hub_id"
-  
+
   attribute {
     name = "hub_id"
     type = "S"
   }
-
+  
   tags = {
     Name        = local.scores_table_name
     Environment = var.environment_name
