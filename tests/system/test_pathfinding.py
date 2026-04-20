29f80c22cd2e152d01a9a8d06654ffdf873a0150
TomerErlich <tomererlich06@gmail.com>
2026-04-20 19:25:09 +1000

    used proper hub ids

--- tests/system/test_pathfinding.py
+++ tests/system/test_pathfinding.py
@@ -5,8 +5,8 @@ from constants import STATUS_BAD_REQUEST, STATUS_OK
 BASE_URL = os.environ["STAGING_BASE_URL"]
 
 def test_pathfinding():
-    hub_id_1 = "1"
-    hub_id_2 = "10"
+    hub_id_1 = "H001"
+    hub_id_2 = "H003"
     
     res = requests.get(f"{BASE_URL}/ese/v1/pathfinding/{hub_id_1}/{hub_id_2}")
     
